0a32716fc913a7bb0a64cd54ac29afb74c9e8290
Marina Natalina <Kiska21mk@ya.ru>
2022-12-07 19:28:02 +0300

    страницы

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const PORT = 3000;
 const app = express();
+const PORT = 3000;
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(__dirname + "/dist"));
 
-app.listen(PORT,  () => {
-    console.log(`Example app listening on port ${PORT}!`);
+app.listen(process.env.PORT || PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
